559717e19768f4d9a11d3a5470deb89cbdcc223e
Sumanta Rakshit <sumantarakshit6299@gmail.com>
2024-08-11 23:17:58 +0530

    frontend login,signup complete

---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { Input } from './ui/input'
+import { Button } from './ui/button';
+import axios from 'axios';
+import { toast } from 'sonner';
+import { Link, useNavigate } from 'react-router-dom';
+import { Loader2 } from 'lucide-react';
+
+
+const Login = () => {
+    const [input, setInput] = useState({
+        email: "",
+        password: ""
+    });
+    const [loading, setLoading] = useState(false);
+    const navigate=useNavigate();
+    const changeEventHandler = (e) => {
+        setInput({ ...input, [e.target.name]: e.target.value });
+
+    }
+    const signupHandler = async (e) => {
+        e.preventDefault();
+        console.log(input);
+        try {
+            setLoading(true);
+            const res = await axios.post('http://localhost:8000/api/v1/user/login', input, {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                withCredentials: true
+            });
+            if (res.data.success) {
+                navigate("/");
+                toast.success(res.data.message);
+                setInput({
+                    
+                    email: "",
+                    password: ""
+                })
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.response.data.message);
+
+        } finally {
+            setLoading(false);
+        }
+    }
+    return (
+        <div className='flex items-center w-screen h-screen justify-center'>
+            <form onSubmit={signupHandler} className='shadow-lg flex flex-col gap-5 p-8'>
+                <div className='my-1'>
+                    <h1 className='text-center font-bold text-xl'>ConneXion</h1>
+                    <p className='text-sm text-center'>Login to see photos & videos from your frind</p>
+                </div>
+                <div>
+                    <span className=' font-medium'>Email</span>
+                    <Input
+                        type='email'
+                        name='email'
+                        value={input.email}
+                        onChange={changeEventHandler}
+                        className='focus-visible:ring-tansparent my-2'
+                    />
+                </div>
+                <div>
+                    <span className=' font-medium'>Password</span>
+                    <Input
+                        type='password'
+                        name='password'
+                        value={input.password}
+                        onChange={changeEventHandler}
+                        className='focus-visible:ring-tansparent my-2'
+                    />
+                </div>
+                {
+                    loading ? (
+                        <Button>
+                            <Loader2 className='mr-2 h-4 w-4 animate-spin'/>
+                            Please Wait
+                        </Button>
+                    ):(<Button type='submit'>Login</Button>)
+                }
+                
+                <span className='text-center'>Doesn't have an account?<Link to="/signup" className='text-blue-600'>Signup</Link></span>
+                
+            </form>
+
+        </div>
+    )
+}
+
+export default Login;
