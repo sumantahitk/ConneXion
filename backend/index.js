b09a99ad3604c4394e77aa82ea8f331ca3e163d8
Sumanta Rakshit <sumantarakshit6299@gmail.com>
2024-08-19 00:04:16 +0530

    modified index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ import path from "path";
 dotenv.config({});
 
 // const app=express();
-const port=process.env.port || 3000;
+const port=process.env.port || 8000;
 
 const __dirname = path.resolve();
 
